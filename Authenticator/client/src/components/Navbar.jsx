8c35aac457a2bbd6f5d9863d8971c5bfe6bc7908
dev98-official <devesh5@deloitte.com>
2022-04-21 01:45:13 +0530

    updated top nav component

--- Authenticator/client/src/components/Navbar.jsx
+++ Authenticator/client/src/components/Navbar.jsx
@@ -1,43 +1,16 @@
 import {React,  Fragment } from "react";
 import { Link } from "react-router-dom";
-import Login from "./Login";
 
 import { Form, FormControl, Button, NavDropdown, Nav, Offcanvas, Container, Navbar } from "react-bootstrap"
 
 function TopNav() {
     return (
         <Fragment>
-            {/* <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark p-4">
-                <Link to="/" className="navbar-brand display-5">Logo</Link>
-                <ul className="navbar-nav display-md-6">
-                    <li className="nav-item">
-                        <Link to="#" className="nav-link">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="#" className="nav-link">Features</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="#" className="nav-link">Pricing</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="#" className="nav-link disabled">Disabled</Link>
-                    </li>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="secondary" id="dropdown-basic">
-                            Dropdown Button
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="/service">Another action</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </ul>
-            </nav> */}
-            <Navbar sticky="top" bg="warning" expand={false} className="p-lg-4">
+            <Navbar sticky="top" bg="warning" expand={false} className="p-sm-4 p-3">
                 <Container fluid>
                     <Link to="/" className="text-decoration-none text-dark">
-                        <Navbar.Brand className="container border p-1">HeavyBite</Navbar.Brand>
+                        <span className="h1 text-dark">Otaku</span>
+                        <span className="ms-2 text-success">&#x25cf;</span>
                     </Link>
                     <Navbar.Toggle aria-controls="offcanvasNavbar" />
                     <Navbar.Offcanvas
@@ -50,8 +23,9 @@ function TopNav() {
                     </Offcanvas.Header>
                     <Offcanvas.Body>
                         <Nav className="justify-content-end flex-grow-1 pe-3">
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
+                        {/* <Nav.Link href="#action1">Home</Nav.Link> */}
+                        <Link className="text-decoration-none text-dark" to="/signup">Signup</Link>
+                        <Link className="text-decoration-none text-dark" to="/">Login</Link>
                         <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
                             <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
@@ -74,8 +48,6 @@ function TopNav() {
                     </Navbar.Offcanvas>
                 </Container>
             </Navbar>
-            <Login/>
-            <Login/>
         </Fragment>
     );
 }
