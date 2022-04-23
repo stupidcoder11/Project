5f3e2cd786d2e2f82f066d15a09bd44062dfe60c
dev98-official <devesh5@deloitte.com>
2022-04-23 12:54:34 +0530

    File to read data as props for components

---
+++ Authenticator/client/src/data.js
@@ -0,0 +1,37 @@
+export const faq = [
+  {
+    question: "Who is your favorite male anime character?",
+    description:
+      "My favourite male anime character is not just one but many! 'L Ryuzaki', 'Naruto Uzumaki', 'Saitama', 'Goku', 'Light Yagami' to name a few",
+  },
+  {
+    question: "What is your favorite anime soundtrack?",
+    description:
+      "My favourite anime soundtrack is 'NEXT TO YOU - PARASYTE THE MAXIM'. It soothes my soul and I can listen to it on and on and on....",
+  },
+  {
+    question: "What was the first anime you ever watched?",
+    description: "Ofcourse it's everyone's favourite a.k.a 'Death Note'",
+  }
+];
+
+export const carouselItems = [
+  {
+    imageUrl: "/images/anime/image-1.jpg",
+    caption: "Tokyo Ghoul",
+    description:
+      "“It's better to be hurt than to hurt others. Nice people can be happy with just that”",
+  },
+  {
+    imageUrl: "/images/anime/image-2.jpg",
+    caption: "Demon Slayer",
+    description:
+      "Head-on battles are simple. Whoever’s stronger and faster wins",
+  },
+  {
+    imageUrl: "/images/anime/image-3.jpg",
+    caption: "My Hero Academia",
+    description:
+      "Whether you win or lose, looking back and learning from your experience is a part of life.",
+  }
+];
