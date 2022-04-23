fcfcca68bd2afba5b4bd2bc07b09fddafc4d7144
dev98-official <devesh5@deloitte.com>
2022-04-23 12:52:36 +0530

    Completed implementation with Offcanvas

--- Authenticator/client/src/components/Navbar.jsx
+++ Authenticator/client/src/components/Navbar.jsx
@@ -1,50 +1,44 @@
 import {React,  Fragment } from "react";
 import { Link } from "react-router-dom";
-
-import { Form, FormControl, Button, NavDropdown, Nav, Offcanvas, Container, Navbar } from "react-bootstrap"
+import { Form, FormControl, Button, Nav, Offcanvas, Container, Navbar } from "react-bootstrap"
 
 function TopNav() {
     return (
         <Fragment>
-            <Navbar sticky="top" bg="warning" expand={false} className="p-sm-4 p-3">
+            <Navbar fixed="top" bg="warning" expand={false} className="p-sm-4 p-3">
                 <Container fluid>
                     <Link to="/" className="text-decoration-none text-dark">
                         <span className="h1 text-dark">Otaku</span>
                         <span className="ms-2 text-success">&#x25cf;</span>
                     </Link>
                     <Navbar.Toggle aria-controls="offcanvasNavbar" />
-                    <Navbar.Offcanvas
-                    id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel"
-                    placement="end"
-                    >
-                    <Offcanvas.Header closeButton>
-                        <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body>
-                        <Nav className="justify-content-end flex-grow-1 pe-3">
-                        {/* <Nav.Link href="#action1">Home</Nav.Link> */}
-                        <Link className="text-decoration-none text-dark" to="/signup">Signup</Link>
-                        <Link className="text-decoration-none text-dark" to="/">Login</Link>
-                        <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                            Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        </Nav>
-                        <Form className="d-flex">
-                        <FormControl
-                            type="search"
-                            placeholder="Search"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success">Search</Button>
-                        </Form>
-                    </Offcanvas.Body>
+                        <Navbar.Offcanvas
+                        id="offcanvasNavbar"
+                        aria-labelledby="offcanvasNavbarLabel"
+                        placement="end"
+                        scroll={true}
+                        >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="offcanvasNavbarLabel">
+                                <span className="display-2">Otaku</span>
+                                <span className="ms-2 text-success">&#x25cf;</span>
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body >
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <Link className="text-decoration-none text-dark mb-2 display-6" to="/posts">Post</Link>
+                            <Link className="text-decoration-none text-warning mb-2 display-6" to="/about">About</Link>
+                            </Nav>
+                            <Form className="d-flex">
+                            <FormControl
+                                type="search"
+                                placeholder="Anime name..."
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-dark">Search</Button>
+                            </Form>
+                        </Offcanvas.Body>
                     </Navbar.Offcanvas>
                 </Container>
             </Navbar>
