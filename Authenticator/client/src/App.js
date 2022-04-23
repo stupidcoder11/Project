a57c50ca24468a48e612e08ed8a31687316e5aad
dev98-official <devesh5@deloitte.com>
2022-04-23 12:53:39 +0530

    Comprehensive modifications

--- Authenticator/client/src/App.js
+++ Authenticator/client/src/App.js
@@ -2,41 +2,43 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Header from './components/Header';
 import Dashboard from './components/Dashboard';
-import { Fragment } from 'react';
-
+import Post from './components/Post';
+import About from './components/About';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route
             path="/"
             element={
-              <Fragment>
+              <>
                 <Header
-                  title="Authentication System"
+                  title="Login Section"
                   description="This is a security system created to protect resource(s) from unauthorized access."
                 />
                 <Login />
-              </Fragment>
+              </>
             }
           />
           <Route
             path="/signup"
             element={
-              <Fragment>
+              <>
                 <Header
-                  title="Authentication System"
+                  title="Signup Section"
                   description="This is a security system created to protect resource(s) from unauthorized access."
                 />
                 <Signup />
-              </Fragment>
+              </>
             }
           />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/posts" element={<Post />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
     </div>
