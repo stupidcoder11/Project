9d19037643111facb2072141688d099dbc153b83
dev98-official <devesh5@deloitte.com>
2022-04-23 12:53:06 +0530

    Custom Header component

--- Authenticator/client/src/components/Header.jsx
+++ Authenticator/client/src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
 function Header(props) {
+    console.count("Header rendered : ")
     return (
         <div className="jumbotron jumbotron-fluid p-3" style={{ background: "#FFCD3C"}}>
             <div className="container text-center">
-            <h1 className="display-4">{props.title}</h1>
-            <p className="lead">
-                {props.description}
-            </p>
+                <h1 className="display-4">{props.title}</h1>
+                <p className="lead">{props.description}</p>
             </div>
       </div>
     );
